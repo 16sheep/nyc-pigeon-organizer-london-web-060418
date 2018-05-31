6e0a09217fd1216058c0e85d9d9cbc2014fbbc5d
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-31 13:17:18 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,42 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_pigeons = {}
+  
+  data.each { |attribute, specification|
+    
+    specification.each {|property, names|
+      
+      names.each {|item|
+        
+        if organized_pigeons.key?(item) == FALSE
+            organized_pigeons[item] = {}
+        end
+        
+        if organized_pigeons[item].key?(attribute) == FALSE
+            organized_pigeons[item][attribute] = []
+        end
+        
+        if specification.value
+        
+      }
+      
+    }
+  }
+  print organized_pigeons
+  organized_pigeons
+end
+
+def organize_schools(schools)
+  organized_hash = {}
+  print schools
+  schools.each do |school, location_hash|
+    location_key = location_hash[:location]
+    if(organized_hash.key?(location_key))
+      organized_hash[location_key].push(school)
+    else
+      organized_hash[location_key] = []
+      organized_hash[location_key].push(school)
+    end
+  end
+
+ organized_hash
 end
